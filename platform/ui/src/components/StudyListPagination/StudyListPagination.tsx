14635b6805375b52f962d225bdcb794b52594b29
Tom Wartmann <tomw@ethz.ch>
2024-05-28 17:22:50 +0200

    set resultsPerPage to 10

--- platform/ui/src/components/StudyListPagination/StudyListPagination.tsx
+++ platform/ui/src/components/StudyListPagination/StudyListPagination.tsx
@@ -16,6 +16,7 @@ const StudyListPagination = ({ onChangePage, currentPage, perPage, onChangePerPa
   };
 
   const ranges = [
+    { value: '10', label: '10' },
     { value: '25', label: '25' },
     { value: '50', label: '50' },
     { value: '100', label: '100' },
