1bb26cd4299a5733215f870f9b8fd0520541b6d1
Tom Wartmann <tomw@ethz.ch>
2024-06-04 12:33:09 +0200

    set report data to the current findings of the original CT scan

--- extensions/text-input-extension/src/components/TextArea.tsx
+++ extensions/text-input-extension/src/components/TextArea.tsx
@@ -1,5 +1,5 @@
 import React,{useState, useEffect} from 'react';
-import {ActionButtons, InputText, Input} from '@ohif/ui'
+import {ActionButtons, Button, ButtonEnums, InputText, Input, Icon, useViewportGrid} from '@ohif/ui'
 import { useNavigate } from 'react-router-dom'
 import {DicomMetadataStore, DisplaySetService} from '@ohif/core'
 import { Enums, annotation as CornerstoneAnnotation, annotations as CornerstoneAnnotations} from '@cornerstonejs/tools';
@@ -17,6 +17,7 @@ import getActiveViewportEnabledElement from '../../../cornerstone/src/utils/getA
 import * as cornerstoneTools from '@cornerstonejs/tools';
 import * as cornerstone from '@cornerstonejs/core';
 import RectangleOverlayViewerTool from '../tools/RectangleOverlayViewerTool';
+//import Icon from '../../../../platform/ui/src/components/Icon'
 
 
 /**
@@ -27,81 +28,169 @@ import RectangleOverlayViewerTool from '../tools/RectangleOverlayViewerTool';
 
 
 function TextArea({servicesManager, commandsManager}){
-    const { measurementService, displaySetService, toolGroupService, segmentationService, viewportGridService} = servicesManager.services;
-    const [initialTextData, setInitialTextData] = useState('');
+    const { measurementService, displaySetService, toolGroupService, segmentationService, viewportGridService, hangingProtocolService} = servicesManager.services;
+    const [reportData, setReportData] = useState('');
     const [textData, setTextData] = useState('');
+
+    const [promptData, setPromptData] = useState('');
     const disabled = false;
+    const [{viewports }] = useViewportGrid();
+
+
+    const handleDisplaySetsChanged = changedDisplaySets => {
+        console.log("handleDisplaySetsChanged")
+        // get studyUIDs of current display
+        const activeDisplaySets = displaySetService.getActiveDisplaySets();
+        const studyInstanceUIDs = activeDisplaySets.map(set => set.StudyInstanceUID);
+        
+        // search for any init_report data
+        const reportList = [];
+        studyInstanceUIDs.forEach(studyInstanceUid => {
+            const studyMetadata = metaData.get('studyMetadata', studyInstanceUid);
+            reportList.push(studyMetadata);
+        });
+        const initialReportElement = reportList.find(result => result && result.hasOwnProperty('initial_report'));
+        const initialReportText = initialReportElement?.['initial_report'] ?? '';
+        
+        setReportData(initialReportText);
+    }; 
 
     useEffect(() => {
-        // Define the event handler function
-        const handleDisplaySetsChanged = changedDisplaySets => {
-            // get studyUIDs of current display
-            const activeDisplaySets = displaySetService.getActiveDisplaySets();
-            const studyInstanceUIDs = activeDisplaySets.map(set => set.StudyInstanceUID);
-            
-            // search for any init_report data
-            const reportList = [];
-            studyInstanceUIDs.forEach(studyInstanceUid => {
-                const studyMetadata = metaData.get('studyMetadata', studyInstanceUid);
-                reportList.push(studyMetadata);
-            });
-            const initialReportElement = reportList.find(result => result && result.hasOwnProperty('initial_report'));
-            const initialReportText = initialReportElement?.['initial_report'] ?? '';
-            
-            setInitialTextData(initialReportText);
-        };
-
+        // run when component is mounted at least once to avoid empty text when closing and reopening tab
+        handleDisplaySetsChanged();
         // Subscribe to the DISPLAY_SETS_CHANGED event
-        const subscription = displaySetService.subscribe(
+        const displaySetSubscription = displaySetService.subscribe(
             displaySetService.EVENTS.DISPLAY_SETS_CHANGED,
             handleDisplaySetsChanged
         );
-
+        
         // Unsubscribe from the event when the component unmounts
         return () => {
-            subscription.unsubscribe(subscription);
+            displaySetSubscription.unsubscribe(displaySetSubscription);
         };
     }, []);
 
-    const handleDoubleClick = () => {
-        // Set the textarea value to the placeholder value, iff empty before
-        if (!textData.trim()) {
-            setTextData(initialTextData);
-        }
-    };
+
+    // change prompt data if certain AI generated image is selected
+    useEffect(() => {
+        const handleGridStateChanged = changedDisplaySets => {
+            // ugly hack since viewport grid updatedes later than GRID_STATE_CHANGED event
+            setTimeout(() => {
+                
+                const viewportState = viewportGridService.getState();
+                
+                const activeViewportId = viewportGridService.getActiveViewportId();
+                
+                const activeViewport = viewportState.viewports.get(activeViewportId);
+
+                const activeDisplaySetInstanceUID = activeViewport.displaySetInstanceUIDs[0];
+                const activeDisplaySets = displaySetService.getActiveDisplaySets();
+                const activeDisplaySet = activeDisplaySets.find(displaySet => displaySet.displaySetInstanceUID === activeDisplaySetInstanceUID);
+                const activeSeriesInstanceUID = activeDisplaySet.SeriesInstanceUID;
+                const activeSeriesStudyMetadata = metaData.get('studyMetadata', activeSeriesInstanceUID);
+                setPromptData(activeSeriesStudyMetadata?.impressions || ''); // set it to empty string if undefined
+
+            },50)
+            
+            
+        }; 
+        
+        // Subscribe to the GRID_STATE_CHANGED event
+        const activeViewportSubscription = viewportGridService.subscribe(
+            viewportGridService.EVENTS.GRID_STATE_CHANGED,
+            handleGridStateChanged
+        );
+        
+        // Unsubscribe from the event when the component unmounts
+        return () => {
+            activeViewportSubscription.unsubscribe(activeViewportSubscription);
+        };
+    }, []);
+
+
+
+    const setPromptDataToReportData = (event) => {
+        console.log(reportData);
+        setPromptData(reportData);
+    }
+    const setReportDataToPromptData = (event) => {
+        setReportData(promptData);
+    }
+
     const submitText = (event) => {
-        console.log(textData);
+        console.log(promptData);
     }
     const clearText = (event) => {
-        setTextData('');
+        setPromptData('');
     }
-    const handleChange = (event) => {
-        setTextData(event.target.value);
+    const handleReportChange = (event) => {
+        setReportData(event.target.value);
+    };
+    const handlePromptChange = (event) => {
+        setPromptData(event.target.value);
     };
 
     //reload images by reloading webside
     const navigate = useNavigate();
     const reloadPage = () => {
-        
-
+        //handleDisplaySetsChanged();
+        console.log("test")
     }
 
 
 
     return (
         <div className="bg-primary-dark">
-            <div className="flex justify-center p-4">
+            <div className="flex flex-col justify-center p-4">
+                <div className="text-primary-main font-bold mb-2">Impressions</div>
                 <textarea  
-                    rows = {6}
+                    rows = {10}
                     label="Enter findings:"
                     className="text-white text-[14px] leading-[1.2] border-primary-main bg-black align-top sshadow transition duration-300 appearance-none border border-inputfield-main focus:border-inputfield-focus focus:outline-none disabled:border-inputfield-disabled rounded w-full py-2 px-3 text-sm text-white placeholder-inputfield-placeholder leading-tight"
                     type="text"
-                    placeholder={initialTextData}
-                    value={textData}
-                    onKeyPress={submitText}
-                    onChange={handleChange}
-                    onDoubleClick={handleDoubleClick}>
+                    value={reportData}
+                    onChange={handleReportChange}
+                    >
             
+                </textarea>
+                <div className="flex justify-center p-4 bg-primary-dark">
+                    {/* <ActionButtons
+                    className="bg-primary-dark mr-4"
+                    actions={[
+                    {
+                        label: 'Save',
+                        onClick: submitText,
+                    }
+                    ]}
+                    disabled={disabled}
+                    /> */}
+                    <Button
+                        onClick={setPromptDataToReportData}
+                        type={ButtonEnums.type.secondary}
+                        size={ButtonEnums.size.small}
+                        className="ml-3"
+                        >
+                        <Icon 
+                            name="arrow-right"
+                            className="transform rotate-90 h-5 w-5"
+                        />
+                    </Button>
+
+                </div>
+                
+            </div>
+            <div className="flex flex-col justify-center p-4">
+                <div className="text-primary-main font-bold mb-2">Text for Image Generation</div>
+                <textarea  
+                    rows = {6}
+                    label="Enter prompt:"
+                    className="text-white text-[14px] leading-[1.2] border-primary-main bg-black align-top sshadow transition duration-300 appearance-none border border-inputfield-main focus:border-inputfield-focus focus:outline-none disabled:border-inputfield-disabled rounded w-full py-2 px-3 text-sm text-white placeholder-inputfield-placeholder leading-tight"
+                    type="text"
+                    value={promptData}
+                    onKeyPress={submitText}
+                    onChange={handlePromptChange}
+                    
+                >
                 </textarea>
             </div>
             <div className="flex justify-center p-4 bg-primary-dark">
@@ -110,7 +199,7 @@ function TextArea({servicesManager, commandsManager}){
                 actions={[
             
                 {
-                    label: 'Generate Image',
+                    label: 'Generate new Image',
                     onClick: submitText,
                 },
                 {
