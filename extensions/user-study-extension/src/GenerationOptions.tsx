c85e5496907e14ae74d60cd0f6ca164d5828fd56
Maggie Chen <maggiechen@Maggies-MacBook-Pro-6.local>
2025-05-16 15:57:30 -0400

    UI updates

--- extensions/user-study-extension/src/GenerationOptions.tsx
+++ extensions/user-study-extension/src/GenerationOptions.tsx
@@ -36,7 +36,7 @@ const GenerationOptions: React.FC<GenerationOptionsProps> = ({
   };
 
   return (
-    <div className="flex items-center font-medium p-1 justify-between">
+    <div className="flex items-center font-medium mb-2 justify-between">
       <span className="mr-2 text-[12px] text-aqua-pale font-semibold font-medium flex items-center">
         {prompt}
       </span>
@@ -57,4 +57,64 @@ const GenerationOptions: React.FC<GenerationOptionsProps> = ({
   );
 };
 
-export default GenerationOptions;
+const Dropdown: React.FC<GenerationOptionsProps> = ({
+  prompt,
+  options,
+  onOptionSelect,
+}) => {
+  const [selectedOption, setSelectedOption] = useState<string | null>(() => null);
+
+  const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setSelectedOption(e.target.value);
+    if (onOptionSelect) {
+      onOptionSelect(e.target.value);
+    }
+  };
+
+  return (
+    <div className="flex items-center mb-2">
+      <span className="mr-2 text-[12px] text-aqua-pale font-semibold flex items-center">
+        {prompt}
+      </span>
+      <select
+        value={selectedOption ?? ''}
+        onChange={handleChange}
+        className="appearance-none border border-secondary-main rounded-md px-2 py-1 bg-primary-dark text-white text-[12px]"
+      >
+        <option value="" disabled>
+          Select...
+        </option>
+        {options.map((option, index) => (
+          <option key={index} value={option}>
+            {option}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+};
+
+
+const GenerateButtons: React.FC = () => (
+  <div className='flex mt-4'>
+    <button
+      key="generate-btn"
+      className="mr-4 bg-primary-main text-white rounded shadow text-sm font-semibold py-1 px-4"
+      // disabled= {modelIsRunning || !isServerRunning || dataIsUploading}
+    >
+      Generate
+    </button>
+    <button key="cancel-btn" className="mr-4 bg-primary-main text-white rounded shadow text-sm font-semibold py-1 px-4">
+      Cancel
+    </button>
+    {/* <ServerStatus
+          modelIsRunning={modelIsRunning}
+          dataIsUploading={dataIsUploading}
+          isServerRunning={isServerRunning}
+          serverUrl={serverUrl}
+        /> */}
+  </div>
+);
+
+
+export { GenerationOptions, Dropdown, GenerateButtons };
--- extensions/user-study-extension/src/tabs/ClassificationPanel.tsx
+++ extensions/user-study-extension/src/tabs/ClassificationPanel.tsx
@@ -1,15 +1,68 @@
 import React from 'react';
-import { Toolbox } from '@ohif/ui-next';
+import DropdownPanel from '../DropdownPanel';
+import { GenerationOptions, Dropdown, GenerateButtons } from '../GenerationOptions';
 
 const ClassificationPanel = ({ commandsManager, servicesManager, extensionManager }) => (
   <div className="my-4">
-    <Toolbox
-      commandsManager={commandsManager}
+    <DropdownPanel
       servicesManager={servicesManager}
-      extensionManager={extensionManager}
-      buttonSectionId="ClassificationToolbox"
-      title="Classification"
-    />
+      dropdownId="classification"
+      title="Classify Pathologies"
+    >
+      <p className='flex p-4 items-center justify-center text-[12px] text-aqua-pale italic'>Select pathologies to classify in the generated CT scan</p>
+
+      <Dropdown
+        prompt="Generation"
+        options={[ "Input", "With", "All", "Generations" ]}
+        onOptionSelect={(option) => console.log(`Selected option: ${option}`)}
+      />
+
+      <GenerationOptions
+        prompt="Pathology to Classify"
+        options={[ "Pleural effusion", "Consoldiation" ]}
+        onOptionSelect={(option) => console.log(`Selected option: ${option}`)}
+      />
+
+      <GenerateButtons/>
+
+       <div className='h-[1px] my-6 mx-4 bg-primary-active rounded rounded-md'></div>
+       {/* <ClassifcationResults/> */}
+    </DropdownPanel>
+
+    <DropdownPanel
+      servicesManager={servicesManager}
+      dropdownId="segment"
+      title="Segment Pathologies"
+    >
+      <p className='flex p-4 items-center justify-center text-[12px] text-aqua-pale italic'>Outline a pathology is the selected CT scan</p>
+      <Dropdown
+        prompt="Generation"
+        options={[ "Input", "With", "All", "Generations" ]}
+        onOptionSelect={(option) => console.log(`Selected option: ${option}`)}
+      />
+    </DropdownPanel>
+
+    <DropdownPanel
+      servicesManager={servicesManager}
+      dropdownId="impression"
+      title="Generate Impression"
+    >
+      <p className='flex p-4 items-center justify-center text-[12px] text-aqua-pale italic'>Select a generation to create an impression</p>
+      <Dropdown
+        prompt="Generation"
+        options={[ "Input", "With", "All", "Generations" ]}
+        onOptionSelect={(option) => console.log(`Selected option: ${option}`)}
+      />
+
+      <GenerateButtons/>
+
+       <div className='h-[1px] my-6 mx-4 bg-primary-active rounded rounded-md'></div>
+       {/* <ImpressionResults/> */}
+    </DropdownPanel>
+
+
+
+
   </div>
 );
 
