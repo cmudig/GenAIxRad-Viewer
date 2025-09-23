97e7fd3d01a62a465f3a0ca01e8601f3ba743fd4
Katelyn Morrison <kcmorris@andrew.cmu.edu>
2025-09-23 11:40:26 -0400

    some fixes for faciltating

--- platform/app/src/firebase.js
+++ platform/app/src/firebase.js
@@ -1,17 +1,16 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
-import { getAnalytics } from "firebase/analytics";
+import { getAnalytics } from 'firebase/analytics';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCjuzeD-Z565tL7vNlBOuxOZpUYW2Az3ec",
-  authDomain: "genai-radiology.firebaseapp.com",
-  databaseURL: "https://genai-radiology-default-rtdb.firebaseio.com",
-  projectId: "genai-radiology",
-  storageBucket: "genai-radiology.appspot.com",
-  messagingSenderId: "287363541296",
-  appId: "1:287363541296:web:91f56424ab4294aeb480f5",
-  measurementId: "G-M17DDMC85V"
+  apiKey: 'AIzaSyCjuzeD-Z565tL7vNlBOuxOZpUYW2Az3ec',
+  authDomain: 'genai-rad-user-study.web.app',
+  projectId: 'genai-radiology',
+  storageBucket: 'genai-rad-user-study.appspot.com',
+  messagingSenderId: '287363541296',
+  appId: '1:287363541296:web:91f56424ab4294aeb480f5',
+  measurementId: 'G-M17DDMC85V',
 };
 
 const app = initializeApp(firebaseConfig);
