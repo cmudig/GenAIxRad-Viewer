fb1cc1a5db2862fad36bbed353f6b0bf8bee3321
Katelyn Morrison <kcmorris@andrew.cmu.edu>
2025-11-25 11:13:12 -0500

    get rid of login functionality for now

--- platform/app/src/components/ProtectedRoute.tsx
+++ platform/app/src/components/ProtectedRoute.tsx
@@ -1,23 +1,8 @@
 // src/components/ProtectedRoute.tsx
 import React from 'react';
-import { useAuthState } from 'react-firebase-hooks/auth';
-import { auth } from '../firebase';
-import { Navigate, useLocation } from 'react-router-dom';
-import { isDemoRoute } from '../utils/demoRoute';
 
+// Auth temporarily disabled; always render the requested route.
 const ProtectedRoute = ({ children }: { children: JSX.Element }) => {
-  const [user, loading] = useAuthState(auth);
-  const location = useLocation();
-  const demoRouteActive = isDemoRoute(location.pathname, location.search);
-
-  if (loading) {
-    return <div>Loading...</div>;
-  }
-
-  if (!user && !demoRouteActive) {
-    return <Navigate to="/login" />;
-  }
-
   return children;
 };
 
