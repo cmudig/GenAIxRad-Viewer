386a86fe2ecf0f360f37d137556c14ff89c0d25c
Katelyn Morrison <kcmorris@andrew.cmu.edu>
2025-12-26 17:48:24 -0500

    updating the similar cases IDs

--- extensions/user-study-extension/src/ExampleComponent.tsx
+++ extensions/user-study-extension/src/ExampleComponent.tsx
@@ -297,7 +297,7 @@ const ExampleComponent: React.FC<ExampleComponentProps> = ({ servicesManager }) 
     }
 
     const HARD_CODED_ACCESSION = '678543127898756';
-    const HARD_CODED_SERIES = ['0000000000037', '0000000000038', '0000000000039'];
+    const HARD_CODED_SERIES = ['0000000000041', '0000000000042', '0000000000043'];
     const patientAccession = String((patientDisplaySet as any)?.AccessionNumber || '').trim();
 
     const resolveHardcoded = () =>
